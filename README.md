9a41c4f5f5614c8a9eea0e161f4c932627337cc4
Yasuhei Nakamura <67479901+yasuhei@users.noreply.github.com>
2025-07-08 22:04:24 -0300

    feat: initial configs db and server

---
+++ README.md
@@ -0,0 +1,5 @@
+comandos para rodar o banco de dados
+
+npm run db:seed
+npx drizzle-kit studio
+npx drizzle migration
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { fastify } from 'fastify';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { fastifyCors } from '@fastify/cors';
+import { env } from './env.ts';
+import { getRoomsRoute } from './routes/get-rooms.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173',
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+  return 'ok';
+});
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT });
